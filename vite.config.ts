fd0447f0f2bebed35b1c5323bb103fbcac831c0f
ulyssis <li.di.china@gmail.com>
2026-07-19 22:31:33 +0200

    modify vite.config.ts to make enable github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '127.0.0.1',
-    port: 5173,
-  },
+    base: 'kid-history-1',
+    plugins: [react()],
+//  server: {
+//    host: '127.0.0.1',
+//    port: 5173,
+//  },
 })
