e6c6593ab6a75de3dd5e7248530e7eaa1954e9f0
ulyssis <li.di.china@gmail.com>
2026-07-19 22:57:57 +0200

    improve vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-    base: 'kid-history-1',
-    plugins: [react()],
-//  server: {
-//    host: '127.0.0.1',
-//    port: 5173,
-//  },
+  base: process.env.GITHUB_ACTIONS ? '/kid-history-1/' : '/',
+  plugins: [react()],
+  server: {
+    host: '127.0.0.1',
+    port: 5173,
+  },
 })
